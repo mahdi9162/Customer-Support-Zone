2f206d31cc8a27513dff03c13ac625ef0bfc8b99
Mahdi Hasan <hasanmahdi6000@gmail.com>
2025-09-26 19:52:34 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Container from './Components/Container/Container';
+import Navbar from './Components/Navbar/Navbar';
+
+function App() {
+  return (
+    <>
+      <Container></Container>
+      <Navbar></Navbar>
+    </>
+  );
+}
+
+export default App;
