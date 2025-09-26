7b50d89a790e2ad832bf2b402f39a47aab48eb17
Mahdi Hasan <hasanmahdi6000@gmail.com>
2025-09-27 03:07:52 +0600

    Add Ticket Cards With Responsiveness

--- src/Components/CsTickets/CsTickets.jsx
+++ src/Components/CsTickets/CsTickets.jsx
@@ -10,12 +10,16 @@ const CsTickets = ({ thePromise }) => {
   return (
     <>
       <Container>
-        <h1 className="mb-4 font-semibold text-2xl">Customer Tickets</h1>
-        <div className="flex gap-6">
-          <div className="w-2/3">
-
+        <h1 className="mb-4 font-semibold text-xl md:text-2xl px-3 md:px-0">Customer Tickets</h1>
+        <div className="flex flex-col-reverse md:flex-row gap-6">
+          <div className="px-3 md:px-0 md:w-2/3">
+            <div className="grid md:grid-cols-2 gap-4">
+              {issuesData.map((issue) => {
+                return <TicketsCards issue={issue}></TicketsCards>;
+              })}
+            </div>
           </div>
-          <div className="h-1/3">
+          <div className="w-1/3 px-3 md:px-0">
             <TaskStatus></TaskStatus>
           </div>
         </div>
--- src/Components/TicketsCards/TicketsCards.jsx
+++ src/Components/TicketsCards/TicketsCards.jsx
@@ -1,28 +1,35 @@
 import React from 'react';
 import { IoCalendar } from 'react-icons/io5';
+import { CgProfile } from 'react-icons/cg';
 
 const TicketsCards = ({ issue }) => {
+  console.log(issue);
+  const { title, status, description, id, priority, customer, createdAt } = issue;
   return (
     <>
-      <div className="p-4 shadow-md rounded-xl bg-white">
+      <div className="h-full flex flex-col p-4 shadow-md rounded-xl bg-white">
         <div className="flex justify-between items-center">
-          <h2>Login Issues - Can't Access Account</h2>
+          <h2 className="font-semibold text-sm md:text-base">{title}</h2>
           <p className="py-1 px-3 bg-[#B9F8CF] rounded-2xl text-sm flex items-center gap-1">
-            <span className="w-4 h-4 rounded-full bg-green-500 inline-block"></span>Open
+            <span className="w-3 h-3 md:w-4 md:h-4 rounded-full bg-green-500 inline-block"></span>
+            {status}
           </p>
         </div>
-        <p className="text-sm text-[#627382] my-3">
-          Customer is unable to log in to their account. They've tried resetting their password multiple times but still...
-        </p>
-        <div className="mt-2 flex justify-between">
-          <div className="flex gap-4">
-            <p className="text-sm text-[#627382]">#1001</p>
-            <p className="text-sm text-red-500">HIGH PRIORITY</p>
+        <p className="text-sm text-[#627382] flex-1 my-3 line-clamp-3">{description}</p>
+        <div className="mt-2 md:flex justify-between">
+          <div className="flex items-center justify-between md:justify-baseline gap-4">
+            <p className="text-sm text-[#627382]">#{id}</p>
+            <p className="text-sm text-red-500">{priority}</p>
           </div>
-          <div className="flex">
-            <p className="text-sm text-[#627382] mr-4">John Smith</p>
-            <IoCalendar color="#627382" size={18} />
-            <span className="text-sm text-[#627382] ml-1">1/15/2024</span>
+          <div className="flex mt-2 justify-between md:justify-baseline">
+            <div className="flex items-center gap-1">
+              <CgProfile color="#627382" size={18} />
+              <p className="text-sm text-[#627382] mr-4">{customer}</p>
+            </div>
+            <div className="flex">
+              <IoCalendar color="#627382" size={18} />
+              <span className="text-sm text-[#627382] ml-1">{createdAt}</span>
+            </div>
           </div>
         </div>
       </div>
